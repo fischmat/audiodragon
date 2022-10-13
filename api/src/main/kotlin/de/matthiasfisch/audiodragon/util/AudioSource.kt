6edebbb2ba638e53ade4f2a67c18fa37760e418b
Matthias Fisch <mail@matthias-fisch.de>
2022-10-13 19:25:48 +0200

    Provide meaningful error message if audio source for ID does not exist

--- api/src/main/kotlin/de/matthiasfisch/audiodragon/util/AudioSource.kt
+++ api/src/main/kotlin/de/matthiasfisch/audiodragon/util/AudioSource.kt
@@ -2,6 +2,7 @@ package de.matthiasfisch.audiodragon.util
 
 import de.matthiasfisch.audiodragon.model.AudioSource
 import de.matthiasfisch.audiodragon.model.getRecordableAudioSources
+import java.lang.IllegalArgumentException
 import java.nio.charset.StandardCharsets
 import java.util.*
 
@@ -9,4 +10,4 @@ typealias AudioSourceId = String
 
 fun AudioSource.getId(): AudioSourceId = Base64.getEncoder().encodeToString("$name|$vendor|$version".toByteArray(StandardCharsets.UTF_8))
 
-fun AudioSourceId.getAudioSource() = getRecordableAudioSources().single { it.getId() == this }
+fun AudioSourceId.getAudioSource() = getRecordableAudioSources().singleOrNull { it.getId() == this } ?: throw IllegalArgumentException("No audio source with id $this exists.")
