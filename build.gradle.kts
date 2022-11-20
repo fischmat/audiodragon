f3eade5603cdadfc246d7fb7d53b1f498adda4a8
Matthias Fisch <mail@matthias-fisch.de>
2022-11-20 17:58:35 +0100

    Structure dependencies in build.gradle.kts
    Signed-off-by: Matthias Fisch <mail@matthias-fisch.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,26 +45,33 @@ allprojects {
 dependencies {
 	implementation(project(":fft"))
 
+	// Spring
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework:spring-messaging")
 	implementation("org.springframework:spring-websocket")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
+	// Utilities
 	implementation("io.reactivex.rxjava2:rxkotlin:2.4.0")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("com.squareup.okhttp3:okhttp:4.10.0")
 	implementation("com.jayway.jsonpath:json-path:2.7.0")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+
+	// Audio file handling
 	implementation("net.jthink:jaudiotagger:3.0.1")
 	implementation("com.cloudburst:java-lame:3.98.4")
+
+	// Database
 	implementation("org.jooq:jooq:3.17.5")
 	implementation("org.flywaydb:flyway-core:9.8.1")
 	implementation("org.xerial:sqlite-jdbc:3.39.3.0")
 
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	// Testing
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
 	testImplementation("io.mockk:mockk:1.13.2")
