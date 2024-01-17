26238ff35984c74fd4c9812183539f474982929f
fischmat <mail@matthias-fisch.de>
2024-01-18 00:02:42 +0100

    Add JRE info to app info endpoint
    Signed-off-by: fischmat <mail@matthias-fisch.de>

--- src/main/kotlin/de/matthiasfisch/audiodragon/controller/InfoController.kt
+++ src/main/kotlin/de/matthiasfisch/audiodragon/controller/InfoController.kt
@@ -1,6 +1,7 @@
 package de.matthiasfisch.audiodragon.controller
 
 import de.matthiasfisch.audiodragon.types.AppInfoDTO
+import de.matthiasfisch.audiodragon.types.JREInfo
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,5 +9,17 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/v1/info")
 class InfoController {
-    @GetMapping fun getAppInfo() = AppInfoDTO(Runtime.getRuntime().maxMemory())
+    @GetMapping fun getAppInfo() = AppInfoDTO(
+        maxMemory = Runtime.getRuntime().maxMemory(),
+        jreInfo = getJREInfo()
+    )
+
+    private fun getJREInfo(): JREInfo {
+        val vendor = System.getProperty("java.vm.vendor")?: "Unknown"
+        val name = System.getProperty("java.vm.name")?: "Unknown"
+        val isSupported =
+            vendor.equals("Oracle Corporation", ignoreCase = true) && name.contains("Java HotSpot", ignoreCase = true) ||
+                    vendor.equals("Oracle Corporation", ignoreCase = true) && name.contains("OpenJDK", ignoreCase = true)
+        return JREInfo(isSupported, name, vendor)
+    }
 }
--- src/main/kotlin/de/matthiasfisch/audiodragon/types/AppInfoDTO.kt
+++ src/main/kotlin/de/matthiasfisch/audiodragon/types/AppInfoDTO.kt
@@ -1,5 +1,12 @@
 package de.matthiasfisch.audiodragon.types
 
 data class AppInfoDTO(
-    val maxMemory: Long
+    val maxMemory: Long,
+    val jreInfo: JREInfo
+)
+
+data class JREInfo(
+    val isSupported: Boolean,
+    val name: String,
+    val vendor: String
 )
