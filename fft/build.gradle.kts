72add1ae19fb4441ea6323a8fe502d8a980ebe48
fischmat <mail@matthias-fisch.de>
2024-10-02 20:29:39 +0200

    chore: Fix source compatibility in fft-subproject
    Signed-off-by: fischmat <mail@matthias-fisch.de>

--- fft/build.gradle.kts
+++ fft/build.gradle.kts
@@ -9,6 +9,12 @@ repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
@@ -16,4 +22,9 @@ dependencies {
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
 }
